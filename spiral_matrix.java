f89ced394391c5d7d3dd4a891500b8141317c669
Sushil <chaubeyshushil000@gmail.com>
2025-11-27 09:29:03 +0530

    Spiral matrix

---
+++ spiral_matrix.java
@@ -0,0 +1,21 @@
+package Basic.programming_java;
+
+public class spiral_matrix {
+    // startRow++ , endRow--, startCol++ , endCol--,
+    // while(=)
+    // print(top)
+  // print(right)
+//  print(bottom)
+//  print(left)
+    public static  void printSpiralMatrix(int matrix[][]){
+        int startRow=0;
+        int startCol=0;
+        int endRow=matrix.length-1;
+        int endCol=matrix[0].length-1;
+
+    }
+    public static void main(String args[]) {
+        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+    }
+}
