533979cee5a9959186b5da284d3495f4a4bd11a4
Sushil <chaubeyshushil000@gmail.com>
2026-01-13 08:41:38 +0530

    add linkedlist

---
+++ linkedlist_1.java
@@ -0,0 +1,13 @@
+package Basic.programming_java;
+
+public class linkedlist_1 {
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static void main(String[] args) {
+        Node x=new Node();
+        System.out.println(x.next);
+
+    }
+}
